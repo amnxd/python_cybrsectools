7cf489851dc8a48fc83e63e3c466012b8ada2fc8
Aman <89342512+amnxd@users.noreply.github.com>
2024-09-22 22:56:16 +0530

    Add files via upload

---
+++ integerWordlistGenerator.py
@@ -0,0 +1,13 @@
+n=0
+m=0
+
+def num(n,m):
+    #using "w" to write in to a new text file and to append to existing file use "a"
+    with open("password1.txt", "w") as f:   
+        for i in range(n, m+1):
+            f.write(str(n) + "\n")
+            n+=1
+
+n = int(input("Enter the proceeding num: "))
+m = int(input("Enter the ending num: "))
+num(n,m)
